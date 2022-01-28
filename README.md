fd5202ed9642b2821e7e0750f671de3b88b8d5f6
bmariem <mariem.borchani@gmail.com>
2022-01-28 16:59:27 +0100

    Initial commit 🎉

---
+++ README.md
@@ -0,0 +1,15 @@
+# Tripadvisor Back
+
+Sending Transactional Email In A NodeJS App Using The Mailgun API
+
+## Installation steps
+
+To be able to install this project on your computer, start by cloning the project than typing the following commands :
+
+### `npm install`
+
+to install all dependencies
+
+### `npm start`
+
+to view the project on your development server : [http://localhost:3000](http://localhost:3000).
---
+++ index.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+const express = require("express");
+const formidable = require("express-formidable");
+const cors = require("cors");
+
+const API_KEY = process.env.API_KEY;
+const DOMAIN = process.env.DOMAIN;
+var mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
+
+const app = express();
+app.use(formidable());
+app.use(cors());
+
+app.post("/form", async (req, res) => {
+  console.log("route /form");
+
+  try {
+    if (
+      req.fields.firstname &&
+      req.fields.lastname &&
+      req.fields.email &&
+      req.fields.text
+    ) {
+      const data = {
+        from: `${req.fields.firstname} ${req.fields.lastname} <me@samples.mailgun.org>`,
+        to: process.env.EMAIL,
+        subject: `Mail from ${req.fields.email}`,
+        text: `${req.fields.text}`,
+      };
+
+      await mailgun.messages().send(data, (error, body) => {
+
+        if (error === undefined) {
+          res.status(200).json({ message: "Email succefully sent 📤" });
+        } else {
+          res.status(400).json({ message: error.message });
+        }
+      });
+    } else {
+      res.status(400).json({ message: "data missing 😞" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json("Welcome :wave:");
+});
+
+app.get("*", (req, res) => {
+  res.status(400).json({ message: "route not found" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server has started 🚀 ");
+});
